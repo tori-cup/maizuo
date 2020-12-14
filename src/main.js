07b00b223b1cde628b086572a6c8e9718a8148a4
yanfeng <2651693982@qq.com>
2020-12-14 17:25:04 +0800

    第一次

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import Vant from 'vant'
-import 'vant/lib/index.css'
+import Vant from "vant";
+import "vant/lib/index.css";
 
-import '../public/css/iconfont/iconfont.css'
-import '../public/css/common.css'
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-Vue.use(MintUI)
+import "../public/css/iconfont/iconfont.css";
+import "../public/css/common.css";
+import MintUI from "mint-ui";
+import "mint-ui/lib/style.css";
+Vue.use(MintUI);
 
-import axios from 'axios'
-Vue.prototype.$http = axios
+import axios from "axios";
+Vue.prototype.$http = axios;
 
-Vue.use(Vant)
-Vue.config.productionTip = false
+Vue.use(Vant);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
